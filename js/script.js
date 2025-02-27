1bd337452ef3d368306f071ea02c516cfe8a86f3
ozodbek2000 <ozodbek.boriboyev2000@gmail.com>
2025-02-27 17:33:26 +0500

    second push

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
-    $('.header__burger').click(function(event) {
-        $('.header__burger,.header__menu').toggleClass('active');
+    $('.main__play').click(function(event) {
+        $(this).toggleClass('active');
+        if ($(this).hasClass('active')) {
+            swiper.autoplay.stop(); // Stop autoplay if active
+        } else {
+            swiper.autoplay.start(); // Start autoplay if not active
+        }
     })
 })
+
+
+// SWIPER
+var swiper = new Swiper('.main__swiper', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+});
