37c38e507b5e64a814fc9b7c40bb84288d50a269
ozodbek2000 <ozodbek.boriboyev2000@gmail.com>
2025-02-28 17:23:02 +0500

    third commit

--- js/script.js
+++ js/script.js
@@ -7,16 +7,34 @@ $(document).ready(function() {
             swiper.autoplay.start(); // Start autoplay if not active
         }
     })
+    $('.qa__dropdown_question').click(function(event) {
+        $(this).parent('.qa__dropdown').toggleClass('active');
+    })
 })
-
 
 // SWIPER
 var swiper = new Swiper('.main__swiper', {
     slidesPerView: 1,
-    spaceBetween: 10,
     loop: true,
     autoplay: {
         delay: 2500,
         disableOnInteraction: false,
     },
+});
+var keySwiper = new Swiper('.key__swiper', {
+    slidesPerView: 7,
+    spaceBetween: 16,
+    navigation: {
+        nextEl: '.key__arrow-right',
+        prevEl: '.key__arrow-left',
+    },
+});
+var newsSwiper = new Swiper('.news__swiper', {
+    slidesPerView: 4,
+    spaceBetween: 16,
+    navigation: {
+        nextEl: '.swiper__arrow-right',
+        prevEl: '.swiper__arrow-left',
+    },
+    loop: true,
 });
